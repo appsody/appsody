8e95d99f4135824d40707fcb2633d02d46c05c35
kewegner <kewegner@us.ibm.com>
2019-06-26 08:34:40 -0500

    added diff check

--- docs-build/deploy.sh
+++ docs-build/deploy.sh
@@ -7,6 +7,9 @@ git clone https://github.com/${GH_ORG}/docs.git
 cd docs
 git checkout -b test${TRAVIS_BUILD_NUMBER}
 
+diff -q ../../build/cli-commands.md ./docs/using-appsody/cli-commands.md
+if [ $? -ne 0 ]
+then
 cp ../../build/cli-commands.md ./docs/using-appsody/cli-commands.md
 
 git add docs/using-appsody/cli-commands.md
@@ -15,6 +18,8 @@ git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
 
 git remote add origin-2 https://${GH_TOKEN}@github.com/${GH_ORG}/docs.git
 git push --set-upstream origin-2 test${TRAVIS_BUILD_NUMBER}
+
+fi
 # clean up
 cd ../..
 rm -rf tmpdocclone
