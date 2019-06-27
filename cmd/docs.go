ca640482b9b894b553e3c36d5a5bc162809d9ce8
kewegner <kewegner@us.ibm.com>
2019-06-27 13:48:20 -0500

    fix copyright

--- cmd/docs.go
+++ cmd/docs.go
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-// Copyright © 2019 NAME HERE IBM Corporation and others.
-=======
 // Copyright © 2019 IBM Corporation and others.
->>>>>>> master
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
