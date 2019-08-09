32c6a229e4d4177f39733ad3d5d4377cc0c07e2b
Terrence Nixa <tnixa@us.ibm.com>
2019-08-09 12:03:55 -0500

    fixed typeo

--- functest/build_test.go
+++ functest/build_test.go
@@ -52,7 +52,7 @@ func TestBuildSimple(t *testing.T) {
 		}
 
 		// create a temporary dir to create the project and run the test
-		projectDir, err := ioutil.TempDir("", "appsody-debug-test")
+		projectDir, err := ioutil.TempDir("", "appsody-build-test")
 		if err != nil {
 			t.Fatal(err)
 		}
