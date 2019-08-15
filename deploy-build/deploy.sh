a8e97288fcfc915baf033aa2cf25d0f90db4d5e6
kewegner <kewegner@us.ibm.com>
2019-08-15 15:19:46 -0500

    added release version and docs info message

--- deploy-build/deploy.sh
+++ deploy-build/deploy.sh
@@ -5,11 +5,11 @@ cd ./tmpclone
 git clone https://${GH_TOKEN}@github.com/${GH_ORG}/homebrew-appsody.git
 
 cd homebrew-appsody
-git checkout -b test${TRAVIS_BUILD_NUMBER}
+git checkout -b test${VERSION}
 cp ../../package/appsody.rb .
 git add appsody.rb
-git commit -m "Travis build: $TRAVIS_BUILD_NUMBER" --author="Kyle G. Christianson <christik@us.ibm.com>"
-git push --set-upstream origin test${TRAVIS_BUILD_NUMBER}
+git commit -m "Travis build: $VERSION" --author="Kyle G. Christianson <christik@us.ibm.com>"
+git push --set-upstream origin test${VERSION}
 # clean up
 cd ../..
 rm -rf tmpclone
