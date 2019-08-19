7304c4e85b84fec7e4c70125b6ae3df985757310
kewegner <47402596+kewegner@users.noreply.github.com>
2019-08-19 16:11:53 -0500

    added release version and docs info message (#193)

--- docs-build/deploy.sh
+++ docs-build/deploy.sh
@@ -11,15 +11,16 @@ diff ../../build/cli-commands.md ./content/docs/using-appsody/cli-commands.md
 if [ $? -ne 0 ]
 then
     set -e
-    git checkout -b test${TRAVIS_BUILD_NUMBER}
+    git checkout -b test${VERSION}
     cp ../../build/cli-commands.md ./content/docs/using-appsody/cli-commands.md
 
     git add content/docs/using-appsody/cli-commands.md
 
-    git commit -m "Travis build: $TRAVIS_BUILD_NUMBER" --author="Kyle G. Christianson <christik@us.ibm.com>"
+    git commit -m "Travis build: $VERSION" --author="Kyle G. Christianson <christik@us.ibm.com>"
 
-    git push --set-upstream origin test${TRAVIS_BUILD_NUMBER}
-
+    git push --set-upstream origin test${VERSION}
+else
+    echo "No changes were found in the appsody doc generation process between releases, no appsody/website branch will be created."
 fi
 # clean up
 cd ../..
