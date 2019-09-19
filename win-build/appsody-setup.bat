84f54f8cfcbde6337e379840085a07f714078d09
Michele Chilanti <chilantim@gmail.com>
2019-09-19 11:46:15 -0500

    Eliminated dups from the PATH env var

--- win-build/appsody-setup.bat
+++ win-build/appsody-setup.bat
@@ -3,31 +3,37 @@
 
 docker ps >nul 2>nul
 IF %ERRORLEVEL% NEQ 0 ECHO [Warning] Docker not running or not installed 
-@echo Adding %~dp0 to your Path environment variable....
+@echo Adding %~dp0 to your Path environment variable if not already present....
 @echo off
+
 setx APPSODY_PATH "%~dp0
 set APPSODY_PATH=%~dp0
-
-
 set lastPathChar=%PATH:~-1%
-
 if NOT "%lastPathChar%" == ";" set "PATH=%PATH%;"
+
 for /F "skip=2 tokens=1,2*" %%N in ('%SystemRoot%\System32\reg.exe query "HKCU\Environment" /v "Path" 2^>nul') do if /I "%%N" == "Path" set "UserPath=%%P"
 IF DEFINED UserPath goto UserPathRead
+REM If no user path env var is set, we just set it to %APPSODY_PATH%
 setx PATH "%%APPSODY_PATH%%
-set PATH=%PATH%%APPSODY_PATH%
-goto :done
+
+goto :SkipSetx
 
 :UserPathRead
+REM If the user path env var is already populated, add %APPSODY_PATH%, unless it is there already
 SET UserPathTest=%UserPath:APPSODY_PATH=NONE%
 REM echo UserPathTest = %UserPathTest%
 REM echo UserPath = %UserPath%
 if NOT "%UserPathTest%" == "%UserPath%" goto SkipSetx
-echo doing setx
+
 set lastUserPathChar=%UserPath:~-1%
 if NOT "%lastUserPathChar%" == ";" set "UserPath=%UserPath%;"
 setx PATH "%UserPath%%%APPSODY_PATH%%
 :SkipSetx
+REM Append the value of %APPSODY_PATH% to the PATH env var, unless it's already there
+CALL SET TestPath=%%PATH:%APPSODY_PATH%=NONE%%
+REM echo TestPath = %TestPath%
+REM echo PATH = %PATH%
+if NOT "%TestPath%" == "%PATH%" goto :done
 set PATH=%PATH%%APPSODY_PATH%
 :done
 
