2d0f8c2abcd9727b9c6ab5a3a0098ad6324716dd
Terrence T Nixa <49254259+tnixa@users.noreply.github.com>
2019-10-02 15:01:35 -0500

    appsody stack validate and package commands (#385)
    * stack validate changes
    
    * stack command changes
    
    * package updates
    
    * changed dev-local to dev.local
    
    * stack validate and package changes
    
    * validate and package changes
    
    * fixed checking for correct directory

---
+++ cmd/stack_validate_utils.go
@@ -0,0 +1,134 @@
+// Copyright © 2019 IBM Corporation and others.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package cmd
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
+
+// RunAppsodyCmdExec runs the appsody CLI with the given args in a new process
+// The stdout and stderr are captured, printed, and returned
+// args will be passed to the appsody command
+// workingDir will be the directory the command runs in
+func RunAppsodyCmdExec(args []string, workingDir string) (string, error) {
+	execDir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		// replace the original working directory when this function completes
+		err := os.Chdir(execDir)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// set the working directory
+	if err := os.Chdir(workingDir); err != nil {
+		return "", err
+	}
+
+	executable, _ := os.Executable()
+
+	cmdArgs := []string{executable, "-v"}
+	cmdArgs = append(cmdArgs, args...)
+	fmt.Println(cmdArgs)
+
+	execCmd := exec.Command(cmdArgs[0], cmdArgs[1:]...)
+	outReader, outWriter, err := os.Pipe()
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		// Make sure to close the writer first or this will hang on Windows
+		outWriter.Close()
+		outReader.Close()
+	}()
+	execCmd.Stdout = outWriter
+	execCmd.Stderr = outWriter
+	outScanner := bufio.NewScanner(outReader)
+	var outBuffer bytes.Buffer
+	go func() {
+		for outScanner.Scan() {
+			out := outScanner.Bytes()
+			outBuffer.Write(out)
+			outBuffer.WriteByte('\n')
+			fmt.Println(string(out))
+		}
+	}()
+
+	err = execCmd.Start()
+	if err != nil {
+		return "", err
+	}
+
+	// replace the original working directory when this function completes
+	err = os.Chdir(execDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = execCmd.Wait()
+
+	return outBuffer.String(), err
+}
+
+// RunDockerCmdExec runs the docker command with the given args in a new process
+// The stdout and stderr are captured, printed, and returned
+// args will be passed to the docker command
+// workingDir will be the directory the command runs in
+func RunDockerCmdExec(args []string) (string, error) {
+
+	cmdArgs := []string{"docker"}
+	cmdArgs = append(cmdArgs, args...)
+	fmt.Println(cmdArgs)
+
+	execCmd := exec.Command(cmdArgs[0], cmdArgs[1:]...)
+	outReader, outWriter, err := os.Pipe()
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		// Make sure to close the writer first or this will hang on Windows
+		outWriter.Close()
+		outReader.Close()
+	}()
+	execCmd.Stdout = outWriter
+	execCmd.Stderr = outWriter
+	outScanner := bufio.NewScanner(outReader)
+	var outBuffer bytes.Buffer
+	go func() {
+		for outScanner.Scan() {
+			out := outScanner.Bytes()
+			outBuffer.Write(out)
+			outBuffer.WriteByte('\n')
+			fmt.Println(string(out))
+		}
+	}()
+
+	err = execCmd.Start()
+	if err != nil {
+		return "", err
+	}
+
+	// replace the original working directory when this function completes
+
+	err = execCmd.Wait()
+
+	return outBuffer.String(), err
+}
