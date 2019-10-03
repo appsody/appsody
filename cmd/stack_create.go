eca926173a389f028ef4b32f6e4bd7df0decf498
Bazif Khan <bazif.khan@ibm.com>
2019-10-03 10:14:35 +0100

    stack create command in progress

--- cmd/stack_create.go
+++ cmd/stack_create.go
@@ -15,25 +15,16 @@
 package cmd
 
 import (
-	"context"
+	"archive/zip"
 	"fmt"
+	"io"
+	"log"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
-	// "github.com/google/go-github/v28/github"	// with go modules enabled (GO111MODULE=on or outside GOPATH)
-
-	"github.com/google/go-github/github" // with go modules disabled
-	"golang.org/x/oauth2"
 )
-
-// Model
-type Package struct {
-	FullName      string
-	Description   string
-	StarsCount    int
-	ForksCount    int
-	LastUpdatedBy string
-}
 
 func newStackCreateCmd(rootConfig *RootCommandConfig) *cobra.Command {
 	var stackCmd = &cobra.Command{
@@ -41,40 +32,103 @@ func newStackCreateCmd(rootConfig *RootCommandConfig) *cobra.Command {
 		Short: "Create a new stack as a copy of an existing stack",
 		Long:  ``,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			context := context.Background()
-			tokenService := oauth2.StaticTokenSource(
-				&oauth2.Token{AccessToken: ""},
-			)
-			tokenClient := oauth2.NewClient(context, tokenService)
 
-			client := github.NewClient(tokenClient)
+			downloadFolderToDisk("https://github.com/appsody/stacks/archive/master.zip", getHome(rootConfig)+"/extract/hello.zip")
 
-			repo, _, err := client.Repositories.Get(context, "appsody", "stacks")
+			unzip(getHome(rootConfig)+"/extract/hello.zip", "output-folder")
+			os.Remove(getHome(rootConfig) + "/extract/hello.zip")
 
-			fmt.Println(repo)
-
+			err := os.Rename("output-folder/stacks-master/incubator/nodejs/", "nodejs")
 			if err != nil {
-				fmt.Printf("Problem in getting repository information %v\n", err)
-				os.Exit(1)
+				log.Fatal(err)
 			}
 
-			pack := &Package{
-				FullName:    *repo.FullName,
-				Description: *repo.Description,
-				ForksCount:  *repo.ForksCount,
-				StarsCount:  *repo.StargazersCount,
+			os.RemoveAll("output-folder")
+
+			err = os.Rename("nodejs", "output-folder")
+			if err != nil {
+				log.Fatal(err)
 			}
 
-			fmt.Printf("%+v\n", pack)
-
-			// // list public repositories for org "github"
-			// opt := &github.RepositoryListByOrgOptions{Type: "public"}
-			// // repos, _, err := client.Repositories.ListByOrg(context.Background(), "appsody", opt)
-			// repos := client.Repositories.Get(context.CancelFunc(), "appsody", "stacks")
-
-			// Info.log(repos)
 			return nil
 		},
 	}
 	return stackCmd
+}
+
+func downloadFolderToDisk(url string, destFile string) error {
+	outFile, err := os.Create(destFile)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	err = downloadFile(url, outFile)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Unzip will decompress a zip archive, moving all files and folders
+// within the zip file (parameter 1) to an output directory (parameter 2).
+func unzip(src string, dest string) ([]string, error) {
+
+	var filenames []string
+
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return filenames, err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+
+		// Store filename/path for returning and using later on
+		fpath := filepath.Join(dest, f.Name)
+
+		// Check for ZipSlip. More Info: http://bit.ly/2MsjAWE
+		if !strings.HasPrefix(fpath, filepath.Clean(dest)+string(os.PathSeparator)) {
+			return filenames, fmt.Errorf("%s: illegal file path", fpath)
+		}
+
+		fileName := strings.Replace(f.Name, "/stacks-master", "", -1)
+		if !strings.HasPrefix(fileName, "stacks-master/incubator/nodejs/") {
+			continue
+		}
+
+		filenames = append(filenames, fpath)
+
+		if f.FileInfo().IsDir() {
+			// Make Folder
+			os.MkdirAll(fpath, os.ModePerm)
+			continue
+		}
+
+		// Make File
+		if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+			return filenames, err
+		}
+
+		outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			return filenames, err
+		}
+
+		rc, err := f.Open()
+		if err != nil {
+			return filenames, err
+		}
+
+		_, err = io.Copy(outFile, rc)
+
+		// Close the file without defer to close before next iteration of loop
+		outFile.Close()
+		rc.Close()
+
+		if err != nil {
+			return filenames, err
+		}
+	}
+	return filenames, nil
 }
