20a2b1425b669e00d07abeb5cfcb37615e7af819
Kyle G. Christianson <christik@us.ibm.com>
2019-10-30 16:18:19 -0500

    Use upstream branch name for git urls if it is different than local branch name

--- cmd/utils.go
+++ cmd/utils.go
@@ -626,6 +626,11 @@ func getGitLabels(config *RootCommandConfig) (map[string]string, error) {
 		labels[ociKeyPrefix+"url"] = gitInfo.RemoteURL
 		labels[ociKeyPrefix+"documentation"] = gitInfo.RemoteURL
 		labels[ociKeyPrefix+"source"] = gitInfo.RemoteURL + "/tree/" + gitInfo.Branch
+		upstreamSplit := strings.Split(strings.Split(gitInfo.Upstream, " ")[0], "/")
+		if len(upstreamSplit) > 1 {
+			labels[ociKeyPrefix+"source"] = gitInfo.RemoteURL + "/tree/" + upstreamSplit[1]
+		}
+
 	}
 
 	var commitInfo = gitInfo.Commit
