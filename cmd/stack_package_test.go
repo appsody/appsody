c47ad397ea89252593bf1688d7cb1e353c4e627b
Cameron McWilliam <cam.mcwilliam@me.com>
2019-11-01 09:01:35 +0000

    Added first test files NOT COMPLETE

---
+++ cmd/stack_package_test.go
@@ -0,0 +1,40 @@
+// Copyright © 2019 IBM Corporation and others.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd_test
+
+import (
+	"testing"
+
+	"github.com/appsody/appsody/cmd/cmdtest"
+)
+
+func TestStackPackage(t *testing.T) {
+
+	args := []string{"stack", "package"}
+
+	_, err := cmdtest.RunAppsodyCmdExec(args, "testdata/nodejs")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	argsRemove := []string{"repo", "remove"}
+	_, err = cmdtest.RunAppsodyCmdExec(argsRemove, "dev.local")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+}
