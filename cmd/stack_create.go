98778daf503102b14962509beda9374addc20350
Sandy Koh <sandykoh99@gmail.com>
2019-11-06 14:01:15 +0000

    lint error

--- cmd/stack_create.go
+++ cmd/stack_create.go
@@ -42,8 +42,7 @@ func newStackCreateCmd(rootConfig *RootCommandConfig) *cobra.Command {
 By default, the new stack is based on the example stack: samples/sample-stack. If you want to use a different stack as the basis for your new stack, use the copy flag to specify the stack you want to use as the starting point. You can use 'appsody list' to see the available stacks.
 
 The stack name must start with a lowercase letter, and can contain only lowercase letters, numbers, or dashes, and cannot end with a dash. The stack name cannot exceed 128 characters.`,
-		Example: 
-`  appsody stack create my-stack  
+		Example: `  appsody stack create my-stack  
   Creates a stack called my-stack, based on the example stack “samples/sample-stack”.
 
   appsody stack create my-stack --copy incubator/nodejs-express  
