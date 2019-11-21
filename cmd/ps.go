cb2b821d727da145ced293a035c0e89174755879
Sandy Koh <sandykoh99@gmail.com>
2019-11-21 09:48:01 +0000

    requested changes

--- cmd/ps.go
+++ cmd/ps.go
@@ -37,9 +37,9 @@ func newPsCmd(rootConfig *RootCommandConfig) *cobra.Command {
 	var psCmd = &cobra.Command{
 		Use:   "ps",
 		Short: "List the Appsody containers running in the local docker environment.",
-		Long: `List all stack-based containers, that are currently running in the local docker envionment. 
+		Long: `List all stack-based containers that are currently running in the local docker envionment. 
 		
-Shows the following information about the currently running Appsody containers: container ID, container name, image and status.`,
+Shows the following information about the Appsody containers that are currently running: container ID, container name, image and status.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
 
 			containers, err := listContainers()
