f584f62d0eef8d941311a20287e7d2e1d9181f73
Sandy Koh <sandykoh99@gmail.com>
2019-11-28 14:27:54 +0000

    only override stack registry if it changes

--- cmd/utils.go
+++ cmd/utils.go
@@ -486,17 +486,18 @@ func getProjectConfig(config *RootCommandConfig) (*ProjectConfig, error) {
 		config.Debug.log("Project stack from config file: ", projectConfig.Stack)
 		imageRepo := config.CliConfig.GetString("images")
 		projectConfig.Stack = stack
+
 		if imageRepo != "docker.io" && !strings.Contains(stack, "dev.local") {
 			config.Debug.log("Image repository set to: ", imageRepo)
 			projectConfig.Stack = imageRepo + "/" + projectConfig.Stack
-		}
-		//Override the stack registry URL
-		projectConfig.Stack, err = OverrideStackRegistry(config.StackRegistry, projectConfig.Stack)
+			//Override the stack registry URL
+			projectConfig.Stack, err = OverrideStackRegistry(config.StackRegistry, projectConfig.Stack)
 
-		if err != nil {
-			return &projectConfig, err
+			if err != nil {
+				return &projectConfig, err
+			}
+			config.Debug.Logf("Project stack after override: %s is: %s", config.StackRegistry, projectConfig.Stack)
 		}
-		config.Debug.Logf("Project stack after override: %s is: %s", config.StackRegistry, projectConfig.Stack)
 		config.ProjectConfig = &projectConfig
 	}
 	return config.ProjectConfig, nil
