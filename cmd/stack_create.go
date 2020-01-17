a2ff358f7398ac3416f762d63403d432d1448f27
Cameron McWilliam <cam.mcwilliam@me.com>
2020-01-17 14:31:38 +0000

    MVP

--- cmd/stack_create.go
+++ cmd/stack_create.go
@@ -15,13 +15,13 @@
 package cmd
 
 import (
-	"archive/zip"
+	"archive/tar"
+	"compress/gzip"
 	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
-	"time"
 
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
@@ -50,7 +50,6 @@ The stack name must start with a lowercase letter, and can contain only lowercas
   appsody stack create my-stack --copy incubator/nodejs-express  
   Creates a stack called my-stack, based on the Node.js Express stack.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			currentTime := time.Now().Format("20060102150405")
 
 			if len(args) < 1 {
 				return errors.New("Required parameter missing. You must specify a stack name")
@@ -84,10 +83,10 @@ The stack name must start with a lowercase letter, and can contain only lowercas
 				}
 			}
 
-			repoAndStack := strings.Split(config.copy, "/")
-
-			repoName := repoAndStack[0]
-			stackName := repoAndStack[1]
+			repoName, stackName, err := parseProjectParm(config.copy, config.RootCommandConfig)
+			if err != nil {
+				return err
+			}
 
 			repoDir := getRepoDir(rootConfig)
 
@@ -122,62 +121,33 @@ The stack name must start with a lowercase letter, and can contain only lowercas
 			if err != nil {
 				return errors.Errorf("Error parsing the repository.yaml file: %v", err)
 			}
-			createStack, err := GetStack(&repoIndex, stackName)
+			createStack, err := getStack(&repoIndex, stackName)
 
 			if err != nil {
 				return err
 			}
 
 			stackSource := createStack.SourceURL
-			extractDir := filepath.Join(getHome(rootConfig), "extract", "repo.zip")
+			extractFilename := stackName + ".tar.gz"
+			extractDir := filepath.Join(getHome(rootConfig), "extract", extractFilename)
 
 			err = downloadFileToDisk(rootConfig.LoggingConfig, stackSource, extractDir, config.Dryrun)
 			if err != nil {
 				return err
 			}
-			_, stackTempDir, err := parseProjectParm(config.copy, config.RootCommandConfig)
+
+			extractFile, err := os.Open(extractDir)
 			if err != nil {
 				return err
 			}
 
-			valid, unzipErr := unzip(rootConfig.LoggingConfig, extractDir, stack, config.copy, config.Dryrun)
-			if unzipErr != nil {
-				return unzipErr
-			}
-
-			if !valid {
-				return errors.Errorf("Invalid stack name: " + config.copy + ". Stack name must be in the format <repo>/<stack>")
+			untarErr := untarSource(stack, extractFile)
+			if untarErr != nil {
+				return untarErr
 			}
 
 			//deleting the stacks repo zip
-			os.Remove(filepath.Join(getHome(rootConfig), "extract", "repo.zip"))
-
-			//moving out the stack which we need
-			if config.Dryrun {
-				config.Info.logf("Dry Run -Skipping moving out of stack: %s from %s", stackTempDir, filepath.Join(stack, "stacks-master", config.copy))
-
-			} else {
-				stackTempDir = ".temp-" + stackTempDir + "-" + currentTime
-
-				err = os.Rename(filepath.Join(stack, "stacks-master", config.copy), stackTempDir)
-				if err != nil {
-					return err
-				}
-			}
-
-			//deleting the folder from which stack is extracted
-			os.RemoveAll(stack)
-
-			// rename the stack to the name which user want
-			if config.Dryrun {
-				config.Info.logf("Dry Run -Skipping renaming of stack from: %s to %s", stackTempDir, stack)
-
-			} else {
-				err = os.Rename(stackTempDir, stack)
-				if err != nil {
-					return err
-				}
-			}
+			os.Remove(extractDir)
 
 			if !config.Dryrun {
 				rootConfig.Info.log("Stack created: ", stack)
@@ -192,74 +162,77 @@ The stack name must start with a lowercase letter, and can contain only lowercas
 	return stackCmd
 }
 
-// Unzip will decompress a zip archive
-// within the zip file (parameter 1) to an output directory (parameter 2).
-func unzip(log *LoggingConfig, src string, dest string, copy string, dryrun bool) (bool, error) {
-	if dryrun {
-		log.Info.logf("Dry Run -Skipping unzip of file: %s from %s", copy, src)
-
-	} else {
-		valid := false
-		r, err := zip.OpenReader(src)
-		if err != nil {
-			return valid, err
-		}
-		defer r.Close()
-
-		for _, f := range r.File {
-
-			// Store filename/path for returning and using later on
-			fpath := filepath.Join(dest, f.Name)
-
-			// Check for ZipSlip. More Info: http://bit.ly/2MsjAWE
-			if !strings.HasPrefix(fpath, filepath.Clean(dest)+string(os.PathSeparator)) {
-				return valid, errors.Errorf("%s: illegal file path", fpath)
-			}
-
-			if f.FileInfo().IsDir() {
-				// Make Folder
-				err := os.MkdirAll(fpath, os.ModePerm)
-				if err != nil {
-					return valid, err
-				}
-				continue
-			}
-
-			// Make File
-			if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
-				return valid, err
-			}
-
-			outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-			if err != nil {
-				return valid, err
-			}
-
-			rc, err := f.Open()
-			if err != nil {
-				return valid, err
-			}
-
-			_, err = io.Copy(outFile, rc)
-
-			// Close the file without defer to close before next iteration of loop
-			outFile.Close()
-			rc.Close()
-
-			if err != nil {
-				return valid, err
-			}
-		}
-		return valid, nil
-	}
-	return true, nil
-}
-
-func GetStack(r *IndexYaml, name string) (IndexYamlStack, error) {
+func getStack(r *IndexYaml, name string) (IndexYamlStack, error) {
 	for _, rf := range r.Stacks {
 		if rf.ID == name {
 			return rf, nil
 		}
 	}
 	return r.Stacks[0], errors.Errorf("Stack not found in Index File")
+}
+
+// taken from https://medium.com/@skdomino/taring-untaring-files-in-go-6b07cf56bc07
+func untarSource(dst string, r io.Reader) error {
+
+	gzr, err := gzip.NewReader(r)
+	if err != nil {
+		return err
+	}
+	defer gzr.Close()
+
+	tr := tar.NewReader(gzr)
+
+	for {
+		header, err := tr.Next()
+
+		switch {
+
+		// if no more files are found return
+		case err == io.EOF:
+			return nil
+
+		// return any other error
+		case err != nil:
+			return err
+
+		// if the header is nil, just skip it (not sure how this happens)
+		case header == nil:
+			continue
+		}
+
+		// the target location where the dir/file should be created
+		target := filepath.Join(dst, header.Name)
+
+		// the following switch could also be done using fi.Mode(), not sure if there
+		// a benefit of using one vs. the other.
+		// fi := header.FileInfo()
+
+		// check the file type
+		switch header.Typeflag {
+
+		// if its a dir and it doesn't exist create it
+		case tar.TypeDir:
+			if _, err := os.Stat(target); err != nil {
+				if err := os.MkdirAll(target, 0755); err != nil {
+					return err
+				}
+			}
+
+		// if it's a file create it
+		case tar.TypeReg:
+			f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
+			if err != nil {
+				return err
+			}
+
+			// copy over contents
+			if _, err := io.Copy(f, tr); err != nil {
+				return err
+			}
+
+			// manually close here after each file operation; defering would cause each file close
+			// to wait until all operations have completed.
+			f.Close()
+		}
+	}
 }
