eda4ae0178988cc769e99c68bdabb7105aacd1fa
Enrique Lacal Bereslawski <Enrique.Lacal.Bereslawski@ibm.com>
2020-02-04 17:03:52 +0000

    Correcty comment

--- cmd/build.go
+++ cmd/build.go
@@ -547,7 +547,7 @@ func updateDeploymentConfig(config *buildCommandConfig, imageName string, labels
 	appsodyApplication.Spec.ApplicationImage = imageName
 
 	// This only applies to the deploy command flow:
-	// - if the namespace doesn't exist in the manifest, and a namespace flag is not set: we write a "default" namespace
+	// - if the namespace doesn't exist in the manifest, and a namespace flag is not set: we do not write a namespace
 	// - if the namespace does exist in the manifest, and a namespace flag is set: we verify that they are the same. If they are not we throw an error.
 	// - if the namespace doesn't exist in the manifest, and a namespace flag is set: we write the value passed an argument with the flag.
 	if appsodyApplication.Namespace == "" && config.namespaceFlagPresent {
