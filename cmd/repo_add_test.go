f7555d2f17c209e746c179fc30747785971fa1ab
Sandy Koh <sandykoh99@gmail.com>
2020-02-17 16:44:48 +0000

    add generic error message

--- cmd/repo_add_test.go
+++ cmd/repo_add_test.go
@@ -99,7 +99,7 @@ func TestRepoAddErrors(t *testing.T) {
 		{"No args", nil, "", "You must specify a repository name and URL"},
 		{"One arg", []string{"reponame"}, "", "You must specify a repository name and URL"},
 		{"No url scheme", []string{"test", "localhost"}, "", "unsupported protocol scheme"},
-		{"Non-existing url", []string{"test", "http://localhost/doesnotexist"}, "", "Error"},
+		{"Non-existing url", []string{"test", "http://localhost/doesnotexist"}, "", "Could not download index"},
 		{"Repo name over 50 characters", []string{"reponametoolongtestreponametoolongtestreponametoolongtest", "http://localhost/doesnotexist"}, "", "must be less than 50 characters"},
 		{"Repo name is invalid", []string{"test!", "http://localhost/doesnotexist"}, "", "Invalid repository name"},
 		{"Repo name already exists", []string{"incubator", "http://localhost/doesnotexist"}, "", "already exists"},
